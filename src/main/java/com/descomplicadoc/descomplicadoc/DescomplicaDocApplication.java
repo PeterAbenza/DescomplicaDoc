dc320dd13f93805bf8e6410230ac75f7391d998c
PeterAbenza <abenzpeter@gmail.com>
2025-05-01 19:05:03 -0300

    Home completa, com tabelas e descriçãos

---
+++ src/main/java/com/descomplicadoc/descomplicadoc/DescomplicaDocApplication.java
@@ -0,0 +1,13 @@
+package com.descomplicadoc.descomplicadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescomplicaDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescomplicaDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/descomplicadoc/descomplicadoc/DescomplicaDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descomplicadoc.descomplicadoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescomplicaDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
